d1831d3cf75bec772dbb24893ddc273e982d2f82
y00eunji <aine0227@naver.com>
2024-09-19 13:58:46 +0900

    :sparkles: 로그아웃 기능 구현

--- src/widgets/Station/Header.tsx
+++ src/widgets/Station/Header.tsx
@@ -1,3 +1,6 @@
+import { usePostLogout } from '@/shared/api/usePostLogout.ts';
+import { useAuthStore } from '@/shared/model/useAuthStore.ts';
+
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { TbLogout } from 'react-icons/tb';
 
@@ -7,13 +10,23 @@ interface IHeaderProps {
 }
 
 export default function Header({ userName, toggleNavigation }: IHeaderProps) {
+  const { mutate } = usePostLogout();
+
+  const handleLogout = () => {
+    mutate(null, {
+      onSuccess: () => {
+        useAuthStore.getState().logout();
+      },
+    });
+  };
+
   return (
     <div className="w-full flex justify-between p-3">
       <RxHamburgerMenu size={25} onClick={toggleNavigation} className="cursor-pointer" />
       <div className="flex justify-center items-center gap-3">
         <img src="/ic-account.png" alt="프로필 이미지" width={30} height={30} />
         <div>{userName}</div>
-        <TbLogout size={25} />
+        <TbLogout size={25} onClick={handleLogout} />
       </div>
     </div>
   );
