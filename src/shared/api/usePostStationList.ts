a2e913dffb156cac24401c22b0177fc1c0d48501
y00eunji <aine0227@naver.com>
2024-09-19 11:53:44 +0900

    :recycle: postStationList 요청 및 응답 타입 변경

--- src/shared/api/usePostStationList.ts
+++ src/shared/api/usePostStationList.ts
@@ -3,57 +3,42 @@ import { axiosRequest, csmsApi } from '@/shared/api/api-config.ts';
 import { useMutation, UseMutationResult } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 
-// enum StationOperatingStatusEnum { // 충전소 운영 상태
-//   OPERATING, // 운영중 1
-//   PAUSE, // 임시중지 2
-//   STOP, // 영구중지 3
-// }
-
 interface IParamsPostStationList extends Record<string, unknown> {
-  descending: boolean; // 내림차순 여부
-  page: number; // 현재 페이지
-  rowsPerPage: number; // 페이지 내 출력 목록 수
-  sortBy?: string; //  정렬 기준 값
-  rowsNumber?: number; // 총 결과 목록 수
+  descending: boolean;
+  page: number;
+  rowsPerPage: number;
+  sortBy?: string;
+  rowsNumber?: number;
 }
 
-// interface IRequestPostStation {
-//   evStationAddress?: string;
-//   evStationName?: string;
-//   operatingInstitutionList?: string[];
-//   operatingStatus?: StationOperatingStatusEnum;
-// }
-
-interface IResponsePostStation {
+export interface IResponsePostStation {
   contents: {
-    no: number; // 결과 순서
-    evStationId: string; // 충전소 ID
-    operatingInstitution: string; // 운영기관
-    evStationName: string; // 충전소 이름
+    no: number;
+    evStationId: string;
+    operatingInstitution: string;
+    evStationName: string;
     address: {
-      // 충전소 주소
-      main: string; // 도로명 주소까지
-      detail: string | null; // 상세 주소
+      main: string;
+      detail: string | null;
     };
     userLimit: {
-      // 사용 정보
-      item: number; // 공통 여부 타입
-      detail: string | null; // 공용 구분 설명
+      item: number;
+      detail: string | null;
     };
-    operatingStatus: number; // 운영 상태 Enum 값
-    registerDate: string; // ISO 8601 format, 등록일자
+    operatingStatus: number;
+    registerDate: string;
   }[];
   pagination: {
-    page: number; // 현재 페이지
-    rowsPerPage: number; // 페이지 내 출력 목록 수
-    sortBy: string[]; // 정렬 기준 값
-    descending: boolean; // 내림차순 여부
-    rowsNumber: number; // 총 결과 목록 수
+    page: number;
+    rowsPerPage: number;
+    sortBy: string[];
+    descending: boolean;
+    rowsNumber: number;
   };
   operatingStatistics: {
-    operating: number; // 운영중 수
-    pause: number; // 임시중지 수
-    stop: number; // 영구중지 수
+    operating: number;
+    pause: number;
+    stop: number;
   };
 }
 
