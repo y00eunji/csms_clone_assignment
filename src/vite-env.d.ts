55ce2f69cbf7366a36d37aa3f158d747b8da8add
y00eunji <aine0227@naver.com>
2024-09-23 09:58:00 +0900

    :label: import meta env 에러 수정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CSMS_ID: string;
+  readonly VITE_CSMS_PASSWORD: string;
+  readonly VITE_CSMS_SERVICE_TYPE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
